dc03cd47f5b0d889288fc20258410362ae8891e5
Mohamad Arastu <mohamad@victor.solutions>
2022-03-14 20:06:27 +0100

    Finished All Testcases

--- src/main/java/MyTestModel.java
+++ src/main/java/MyTestModel.java
@@ -1,9 +1,42 @@
 public class MyTestModel {
-    public static boolean checkPassword(String password) {
-        return password.length() >= 8 && password.length() <= 25;
+    public static boolean checkIfUpper(String input) {
+        for (char chr : input.toCharArray())
+            if (Character.isUpperCase(chr))
+                return true;
+        return false;
     }
 
-    public static void main(String[] args) {
-        System.out.println("Hello Test Test ");
+    public static boolean checkIfLower(String input) {
+        for (char chr : input.toCharArray())
+            if (Character.isLowerCase(chr))
+                return true;
+        return false;
+    }
+
+    public static boolean checkLength(String input) {
+        return input.length() >= 8 && input.length() <= 25;
+    }
+
+    public static boolean checkIsDigit(String input) {
+        for (char chr : input.toCharArray())
+            if (Character.isDigit(chr))
+                return true;
+        return false;
+    }
+
+    public static boolean checkIfSpecialCharacter(String input) {
+        char[] specialSymbols = new char[]{'@', '#', '$', '?', '!', '%', '/', '(', ')'};
+        for (char chr : input.toCharArray())
+            for (char chrArr : specialSymbols)
+                if (chr == chrArr)
+                    return true;
+        return false;
+    }
+
+    public static boolean checkPassword(String password) {
+        return checkLength(password)
+                && (checkIfUpper(password) && checkIfLower(password))
+                && (checkIsDigit(password))
+                && (checkIfSpecialCharacter(password));
     }
 }
--- src/test/java/MyTestModelTest.java
+++ src/test/java/MyTestModelTest.java
@@ -1,10 +1,98 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MyTestModelTest {
     @Test
-    void checkPassword_too_short(){
-        boolean actual = MyTestModel.checkPassword("1234");
+    void checkPasswordTooShort() {
+        boolean actual = MyTestModel.checkLength("1234567");
+        boolean expected = false;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void checkPasswordCorrectLength() {
+        boolean actual = MyTestModel.checkLength("12345678910");
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void checkPasswordTooLong() {
+        boolean actual = MyTestModel.checkLength("12345678123456781234567812345678");
+        boolean expected = false;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void checkPasswordUpperCase() {
+        boolean actual = MyTestModel.checkIfUpper("KKKKKKKKKKKKKKKKK");
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void checkPasswordLowerCase() {
+
+        boolean actual = MyTestModel.checkIfLower("kkkkkkkkkkkkkkkkkkkkk");
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void checkPasswordUpperCase_False() {
+        boolean actual = MyTestModel.checkIfUpper("kkkkkkkk");
+        boolean expected = false;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void checkPasswordLowerCase_False() {
+
+        boolean actual = MyTestModel.checkIfLower("KKKKKKK");
+        boolean expected = false;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void hasDigit() {
+        boolean actual = MyTestModel.checkIsDigit("asd123");
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void hasNoDigit() {
+        boolean actual = MyTestModel.checkIsDigit("asdsde");
+        boolean expected = false;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void hasSpecialSymbol() {
+        boolean actual = MyTestModel.checkIfSpecialCharacter("das@!$2()");
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void hasNoSpecialSymbol() {
+        boolean actual = MyTestModel.checkIfSpecialCharacter("das123");
+        boolean expected = false;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void iscorrectPassword() {
+        boolean actual = MyTestModel.checkPassword("123dasDkwW@($!");
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void isNotCorrectPassword() {
+        boolean actual = MyTestModel.checkPassword("asldklaskdlakksdlkasldkalskdlaksldklkalskd@23SDK");
+        boolean expected = false;
+        assertEquals(expected, actual);
     }
 }
